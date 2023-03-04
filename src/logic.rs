3379cd19c7311209a465f22bca0cc9211e4d573a
Emilien Breton <bricktech2000@gmail.com>
2023-03-04 11:41:00 -0500

    rust format

--- src/logic.rs
+++ src/logic.rs
@@ -24,119 +24,119 @@ const PLAYER_COUNT: usize = 4;
 
 pub struct DenseBoard<T>
 where
-    T: Clone + Copy,
+  T: Clone + Copy,
 {
-    board: [T; (BOARD_SIZE + 2) * (BOARD_SIZE + 2)],
+  board: [T; (BOARD_SIZE + 2) * (BOARD_SIZE + 2)],
 }
 
 impl<T> DenseBoard<T>
 where
-    T: Clone + Copy,
+  T: Clone + Copy,
 {
-    fn init(default: T) -> DenseBoard<T> {
-        DenseBoard {
-            board: [default; (BOARD_SIZE + 2) * (BOARD_SIZE + 2)],
-        }
+  fn init(default: T) -> DenseBoard<T> {
+    DenseBoard {
+      board: [default; (BOARD_SIZE + 2) * (BOARD_SIZE + 2)],
     }
+  }
 
-    fn get_xy(&self, x: isize, y: isize) -> T {
-        self.board[(y + 1) as usize * (BOARD_SIZE + 2) + (x + 1) as usize]
-    }
+  fn get_xy(&self, x: isize, y: isize) -> T {
+    self.board[(y + 1) as usize * (BOARD_SIZE + 2) + (x + 1) as usize]
+  }
 
-    fn get_xy_mut(&mut self, x: usize, y: usize) -> &mut T {
-        &mut self.board[(y + 1) * (BOARD_SIZE + 2) + (x + 1)]
-    }
+  fn get_xy_mut(&mut self, x: usize, y: usize) -> &mut T {
+    &mut self.board[(y + 1) * (BOARD_SIZE + 2) + (x + 1)]
+  }
 
-    fn get_coord(&self, c: Coord) -> T {
-        self.get_xy(c.x as isize, c.y as isize)
-    }
+  fn get_coord(&self, c: Coord) -> T {
+    self.get_xy(c.x as isize, c.y as isize)
+  }
 
-    fn get_coord_mut(&mut self, c: Coord) -> &mut T {
-        self.get_xy_mut(c.x as usize, c.y as usize)
-    }
+  fn get_coord_mut(&mut self, c: Coord) -> &mut T {
+    self.get_xy_mut(c.x as usize, c.y as usize)
+  }
 }
 
 struct GameState {
-    turn: u16,
-    area: DenseBoard<u16>,
-    heads: [Coord; PLAYER_COUNT],
+  turn: u16,
+  area: DenseBoard<u16>,
+  heads: [Coord; PLAYER_COUNT],
 }
 
 impl GameState {
-    fn new(turn: u32, board: &Board) -> GameState {
-        let mut area = DenseBoard::init(0);
-        GameState {
-            turn: turn as u16,
-            area,
-        }
+  fn new(turn: u32, board: &Board) -> GameState {
+    let mut area = DenseBoard::init(0);
+    GameState {
+      turn: turn as u16,
+      area,
     }
+  }
 }
 
 // info is called when you create your Battlesnake on play.battlesnake.com
 // and controls your Battlesnake's appearance
 // TIP: If you open your Battlesnake URL in a browser you should see this data
 pub fn info() -> Value {
-    info!("INFO");
+  info!("INFO");
 
-    return json!({
-        "apiversion": "1",
-        "author": "", // TODO: Your Battlesnake Username
-        "color": "#e9ecef", // TODO: Choose color
-        "head": "default", // TODO: Choose head
-        "tail": "default", // TODO: Choose tail
-    });
+  return json!({
+      "apiversion": "1",
+      "author": "", // TODO: Your Battlesnake Username
+      "color": "#e9ecef", // TODO: Choose color
+      "head": "default", // TODO: Choose head
+      "tail": "default", // TODO: Choose tail
+  });
 }
 
 // start is called when your Battlesnake begins a game
 pub fn start(_game: &Game, _turn: &u32, _board: &Board, _you: &Battlesnake) {
-    info!("GAME START");
+  info!("GAME START");
 }
 
 // end is called when your Battlesnake finishes a game
 pub fn end(_game: &Game, _turn: &u32, _board: &Board, _you: &Battlesnake) {
-    info!("GAME OVER");
+  info!("GAME OVER");
 }
 
 // move is called on every turn and returns your next move
 // Valid moves are "up", "down", "left", or "right"
 // See https://docs.battlesnake.com/api/example-move for available data
 pub fn get_move(_game: &Game, turn: &u32, _board: &Board, you: &Battlesnake) -> Option<Direction> {
-    // We've included code to prevent your Battlesnake from moving backwards
-    let my_head = &you.body[0]; // Coordinates of your head
-    let my_neck = &you.body[1]; // Coordinates of your "neck"
+  // We've included code to prevent your Battlesnake from moving backwards
+  let my_head = &you.body[0]; // Coordinates of your head
+  let my_neck = &you.body[1]; // Coordinates of your "neck"
 
-    let mut is_move_safe: HashMap<_, _> = vec![
-        (Direction::Up, my_neck.y <= my_head.y),
-        (Direction::Down, my_neck.y >= my_head.y),
-        (Direction::Left, my_neck.x >= my_head.x),
-        (Direction::Right, my_neck.x <= my_head.x),
-    ]
+  let mut is_move_safe: HashMap<_, _> = vec![
+    (Direction::Up, my_neck.y <= my_head.y),
+    (Direction::Down, my_neck.y >= my_head.y),
+    (Direction::Left, my_neck.x >= my_head.x),
+    (Direction::Right, my_neck.x <= my_head.x),
+  ]
+  .into_iter()
+  .collect();
+
+  // TODO: Step 1 - Prevent your Battlesnake from moving out of bounds
+  // let board_width = &board.width;
+  // let board_height = &board.height;
+
+  // TODO: Step 2 - Prevent your Battlesnake from colliding with itself
+  // let my_body = &you.body;
+
+  // TODO: Step 3 - Prevent your Battlesnake from colliding with other Battlesnakes
+  // let opponents = &board.snakes;
+
+  // Are there any safe moves left?
+  let safe_moves = is_move_safe
     .into_iter()
-    .collect();
-    
-    // TODO: Step 1 - Prevent your Battlesnake from moving out of bounds
-    // let board_width = &board.width;
-    // let board_height = &board.height;
+    .filter(|&(_, v)| v)
+    .map(|(k, _)| k)
+    .collect::<Vec<_>>();
 
-    // TODO: Step 2 - Prevent your Battlesnake from colliding with itself
-    // let my_body = &you.body;
+  // Choose a random move from the safe ones
+  let chosen = safe_moves.choose(&mut rand::thread_rng()).unwrap().clone();
 
-    // TODO: Step 3 - Prevent your Battlesnake from colliding with other Battlesnakes
-    // let opponents = &board.snakes;
+  // TODO: Step 4 - Move towards food instead of random, to regain health and survive longer
+  // let food = &board.food;
 
-    // Are there any safe moves left?
-    let safe_moves = is_move_safe
-        .into_iter()
-        .filter(|&(_, v)| v)
-        .map(|(k, _)| k)
-        .collect::<Vec<_>>();
-    
-    // Choose a random move from the safe ones
-    let chosen = safe_moves.choose(&mut rand::thread_rng()).unwrap().clone();
-
-    // TODO: Step 4 - Move towards food instead of random, to regain health and survive longer
-    // let food = &board.food;
-
-    info!("MOVE {}: {:?}", turn, chosen);
-    return Some(chosen);
+  info!("MOVE {}: {:?}", turn, chosen);
+  return Some(chosen);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-
 #[macro_use]
 extern crate rocket;
 
@@ -7,7 +6,7 @@ use rocket::fairing::AdHoc;
 use rocket::http::Status;
 use rocket::serde::{json::Json, Deserialize};
 use serde::Serialize;
-use serde_json::{Value, json};
+use serde_json::{json, Value};
 use std::collections::HashMap;
 use std::env;
 
@@ -18,118 +17,119 @@ mod logic;
 
 #[derive(Deserialize, Serialize, Debug)]
 pub struct Game {
-    id: String,
-    ruleset: HashMap<String, Value>,
-    timeout: u32,
+  id: String,
+  ruleset: HashMap<String, Value>,
+  timeout: u32,
 }
 
 #[derive(Deserialize, Serialize, Debug)]
 pub struct Board {
-    height: u32,
-    width: u32,
-    food: Vec<Coord>,
-    snakes: Vec<Battlesnake>,
-    hazards: Vec<Coord>,
+  height: u32,
+  width: u32,
+  food: Vec<Coord>,
+  snakes: Vec<Battlesnake>,
+  hazards: Vec<Coord>,
 }
 
 #[derive(Deserialize, Serialize, Debug)]
 pub struct Battlesnake {
-    id: String,
-    name: String,
-    health: u32,
-    body: Vec<Coord>,
-    head: Coord,
-    length: u32,
-    latency: String,
-    shout: Option<String>,
+  id: String,
+  name: String,
+  health: u32,
+  body: Vec<Coord>,
+  head: Coord,
+  length: u32,
+  latency: String,
+  shout: Option<String>,
 }
 
 #[derive(Deserialize, Serialize, Debug)]
 pub struct Coord {
-    x: u32,
-    y: u32,
+  x: u32,
+  y: u32,
 }
 
 #[derive(Deserialize, Serialize, Debug)]
 pub struct GameState {
-    game: Game,
-    turn: u32,
-    board: Board,
-    you: Battlesnake,
+  game: Game,
+  turn: u32,
+  board: Board,
+  you: Battlesnake,
 }
 
 #[derive(Deserialize, Serialize, Debug, PartialEq, Eq, Hash, Clone, Copy)]
 #[serde(rename_all = "lowercase")]
 pub enum Direction {
-    Up,
-    Down,
-    Left,
-    Right
+  Up,
+  Down,
+  Left,
+  Right,
 }
 
 #[get("/")]
 fn handle_index() -> Json<Value> {
-    Json(logic::info())
+  Json(logic::info())
 }
 
 #[post("/start", format = "json", data = "<start_req>")]
 fn handle_start(start_req: Json<GameState>) -> Status {
-    logic::start(
-        &start_req.game,
-        &start_req.turn,
-        &start_req.board,
-        &start_req.you,
-    );
+  logic::start(
+    &start_req.game,
+    &start_req.turn,
+    &start_req.board,
+    &start_req.you,
+  );
 
-    Status::Ok
+  Status::Ok
 }
 
 #[post("/move", format = "json", data = "<move_req>")]
 fn handle_move(move_req: Json<GameState>) -> Json<Value> {
-    let direction = logic::get_move(
-        &move_req.game,
-        &move_req.turn,
-        &move_req.board,
-        &move_req.you,
-    ).unwrap_or(Direction::Up);
+  let direction = logic::get_move(
+    &move_req.game,
+    &move_req.turn,
+    &move_req.board,
+    &move_req.you,
+  )
+  .unwrap_or(Direction::Up);
 
-    Json(json!({ "move": direction }))
+  Json(json!({ "move": direction }))
 }
 
 #[post("/end", format = "json", data = "<end_req>")]
 fn handle_end(end_req: Json<GameState>) -> Status {
-    logic::end(&end_req.game, &end_req.turn, &end_req.board, &end_req.you);
+  logic::end(&end_req.game, &end_req.turn, &end_req.board, &end_req.you);
 
-    Status::Ok
+  Status::Ok
 }
 
 #[launch]
 fn rocket() -> _ {
-    // Lots of web hosting services expect you to bind to the port specified by the `PORT`
-    // environment variable. However, Rocket looks at the `ROCKET_PORT` environment variable.
-    // If we find a value for `PORT`, we set `ROCKET_PORT` to that value.
-    if let Ok(port) = env::var("PORT") {
-        env::set_var("ROCKET_PORT", &port);
-    }
+  // Lots of web hosting services expect you to bind to the port specified by the `PORT`
+  // environment variable. However, Rocket looks at the `ROCKET_PORT` environment variable.
+  // If we find a value for `PORT`, we set `ROCKET_PORT` to that value.
+  if let Ok(port) = env::var("PORT") {
+    env::set_var("ROCKET_PORT", &port);
+  }
 
-    // We default to 'info' level logging. But if the `RUST_LOG` environment variable is set,
-    // we keep that value instead.
-    if env::var("RUST_LOG").is_err() {
-        env::set_var("RUST_LOG", "info");
-    }
+  // We default to 'info' level logging. But if the `RUST_LOG` environment variable is set,
+  // we keep that value instead.
+  if env::var("RUST_LOG").is_err() {
+    env::set_var("RUST_LOG", "info");
+  }
 
-    env_logger::init();
+  env_logger::init();
 
-    info!("Starting Battlesnake Server...");
+  info!("Starting Battlesnake Server...");
 
-    rocket::build()
-        .attach(AdHoc::on_response("Server ID Middleware", |_, res| {
-            Box::pin(async move {
-                res.set_raw_header("Server", "battlesnake/github/starter-snake-rust");
-            })
-        }))
-        .mount(
-            "/",
-            routes![handle_index, handle_start, handle_move, handle_end],
-        )
+  rocket::build()
+    .attach(AdHoc::on_response("Server ID Middleware", |_, res| {
+      Box::pin(async move {
+        res.set_raw_header("Server", "battlesnake/github/starter-snake-rust");
+      })
+    }))
+    .mount(
+      "/",
+      routes![handle_index, handle_start, handle_move, handle_end],
+    )
 }
